954c55bafa5fd6fcfaee2cc41e61bef0637f28f3
Elodie Bassibey <elo@iMac-de-Elodie.local>
2020-07-08 15:14:29 +0200

    CSS home suite

--- templates/base.php
+++ templates/base.php
@@ -15,10 +15,10 @@
 
 <body>
     <header>
-        <div id="topbar col-12">
-            <div class="topbar-container">
+        <div id="topbar" class="col-12 ">
+            <div class="topbar-container d-none d-md-flex">
 
-                <ul class="topbar_info">
+                <ul class="topbar-info">
                     <li><a href="index.php"><img id="logo" src="public/images/site/logo.jpg" alt="akiba-store logo"></a></li>
                     <li><img class="topbar-icon" src="public/icones/phone.gif" alt="phond"> 05 61 38 72 54</li>
                     <li><img class="topbar-icon" src="public/icones/mail.gif" alt="mail"> akiba.store31@gmail.com</li>
@@ -28,15 +28,33 @@
                     <li><a href="">Panier <img class="topbar-icon" src="public/icones/basket.gif" alt="basket"></a></li>
                 </ul>
             </div>
+            <div class="topbar-container d-flex d-sm-flex d-md-none">
+                <li><a href="index.php"><img id="logo" src="public/images/site/logo.jpg" alt="akiba-store logo"></a></li>
+            </div>
         </div>
         <nav class="col-12">
-            <div class="nav-container">
+            <div class="nav-container topbar-container d-none d-md-flex">
                 <ul class="nav">
                     <li class="nav-item"><a href="">Mangas</a></li>
                     <li class="nav-item"><a href="">Figurines</a></li>
                     <li class="nav-item"><a href="">Cosplay/Vêtements</a></li>
                     <li class="nav-item"><a href="">Evénements</a></li>
                     <li class="nav-item"><a href="">Formules</a></li>
+                </ul>
+            </div>
+            <div class="nav-container d-flex d-sm-flex d-md-none">
+
+                <ul class="nav">
+                    <li class="nav-item"><a href="">Mangas</a></li>
+                    <li class="nav-item"><a href="">Figurines</a></li>
+                    <li class="nav-item"><a href="">Cosplay/Vêtements</a></li>
+                    <li class="nav-item"><a href="">Evénements</a></li>
+                    <li class="nav-item"><a href="">Formules</a></li>
+                </ul>
+
+                <ul class="topbar-link">
+                    <li><a href="">S'enregistrer <img class="topbar-icon" src="public/icones/registration.gif" alt="registration"></a></li>
+                    <li><a href="">Panier <img class="topbar-icon" src="public/icones/basket.gif" alt="basket"></a></li>
                 </ul>
             </div>
 
@@ -46,18 +64,18 @@
     <?= $content ?>
 
     <footer>
-    <section class="row container col-xs-12">
-            <ul class="col-sm-offset-1 col-sm-5 col-xs-12">
-                <li><a href="index.php?route=mentions"> Mentions Légales</a></li>
-                <li><a href="index.php?route=confidentiality">Politique de confidentialité</a></li>
+        <section id="footer-link" class="row container col-xs-12">
+            <ul class="col-sm-6 col-xs-12">
+                <li><a href=""> Mentions Légales</a></li>
+                <li><a href="y">Politique de confidentialité</a></li>
             </ul>
-            <ul class="col-sm-5 col-xs-12">
-                <li><a href="index.php">Accueil</a></li>
-                <li><a href="index.php?route=APropos">A propos de l'auteur</a></li>
+            <ul class="col-sm-6 col-xs-12">
+                <li><a href="">Accueil</a></li>
+                <li><a href="">Conditions Générales de Vente</a></li>
             </ul>
         </section>
-        <section id="copyright" class="col-xs-12">
-            <p>© 2020 Site par Elodie BASSIBEY - Formation OpenClassrooms <a href="https://openclassrooms.com/fr/paths/48-developpeur-web-junior"> Développeur Web Junior</a></p>
+        <section id="copyright" class="col-xs-12 container-fluid">
+            <p>© 2020 Site par Elodie BASSIBEY</p>
         </section>
     </footer>
 </body>
